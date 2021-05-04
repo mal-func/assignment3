01fdfcbeac8c4a122e5dba1031a911271764bb7d
mal-func <76743189+mal-func@users.noreply.github.com>
2021-05-04 09:53:52 +0530

    Create qes2.py

---
+++ qes2.py
@@ -0,0 +1,10 @@
+class DataMember:
+  def __init__(self):
+    __data1 = 'Data Member 1'
+    __data2 = 'Data Member 2'
+    print(__data1)
+    print(__data2)
+def getData():
+  ob=DataMember()
+
+getData()
